57db70b3b44808c13555da008e4e3bf2928358ba
Kebron <Kebronjrdaniel@gmail.com>
2023-01-30 11:21:02 +0300

    inital commit

---
+++ src/test/java/com/example/jokeGenerator/JokeGenerator/JokeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jokeGenerator.JokeGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
